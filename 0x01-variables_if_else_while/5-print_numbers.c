2161cedf855cf1d9b90ecac13ad036ed9d80cdee
Jesse Joojo Hub <jessejoojo01@gmai.com>
2023-03-17 19:48:55 -0700

    5pp

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints numbers from 0 to 9.
- *
- * return: Always 0 (Success)
+ * main - Prints numbers from 0 to 9.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
